c81f323b717dd1bb0b27e43e751bb305ff822db8
Eugene Storm <storm.eugene@yandex.ru>
2021-02-22 23:37:16 +0300

    Create 0001.py

---
+++ 0001.py
@@ -0,0 +1,9 @@
+# Solving tasks to competitive programming from the site https://acmp.ru
+# https://acmp.ru/index.asp?main=task&id_task=1
+# Author of solutions: Eugene Storm
+# 
+# Task №1
+# ID 0001
+
+a, b = (int(i) for i in input().split())
+print(a + b)
