589a06ee29fc5af8f1520d06707cad278d09c058
Eugene Storm <storm.eugene@yandex.ru>
2021-02-26 00:39:35 +0300

    method for quickly squaring numbers ending in 5

---
+++ 0003.py
@@ -0,0 +1,12 @@
+# Solving tasks to competitive programming from the site https://acmp.ru
+# https://acmp.ru/index.asp?main=task&id_task=3
+# Author of solutions: Eugene Storm
+# 
+# Task №3
+# ID 0003
+
+a = int(input())//10
+if a!=0:
+    print(a*(a+1),25,sep='')
+else:
+    print(25)
