246a914a134663a65615485862a6cd66108523d8
Eugene Storm <storm.eugene@yandex.ru>
2022-12-02 22:56:44 +0100

    ID 0004 - The game.

---
+++ 0004.py
@@ -0,0 +1,9 @@
+# Solving tasks to competitive programming from the site https://acmp.ru
+# https://acmp.ru/index.asp?main=task&id_task=4
+# Author of solutions: Eugene Storm
+# 
+# Task №4
+# ID 0004
+
+a=int(input())
+print(a*100+90+9-a)
