803444333445980e3786ab3bc9a734eae16a660c
Eugene Storm <storm.eugene@yandex.ru>
2022-12-11 14:56:37 +0100

    ID 0007 (ver.2) - Gold of the ABBA Tribe.

---
+++ 0007-2.py
@@ -0,0 +1,14 @@
+# Solving tasks to competitive programming from the site https://acmp.ru
+# https://acmp.ru/index.asp?main=task&id_task=7
+# Author of solutions: Eugene Storm
+# 
+# Task №7
+# ID 0007
+# Version - 2
+# Completion time - 0,031 sec.
+
+
+
+main = [int(elem) for elem in list(''.join(input()).split(' ')) if elem != '']
+
+print(max(main))
